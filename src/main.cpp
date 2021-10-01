ea5a2606ab03e0ce34680f07c2d380ef813774c4
Steffan Sølvsten <201505832@post.au.dk>
2021-10-01 17:27:26 +0200

    Add small and simple program in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,35 @@
 // std imports
+#include <iostream>
 #include <vector>
 #include <functional>
 
 // quicksort import
 #include <qs/sort.h>
 
+template<typename T>
+void print_vector(const std::vector<T> &v)
+{
+  std::cout << "vector: { ";
+  for (const T &t : v) { std::cout << t << " "; }
+  std::cout << "}" << std::endl << std::endl;
+}
+
 int main(int argc, char* argv[]) {
   // ===== Your code starts here =====
 
+  std::vector<int> v = {0, 1, 2, 3, 4, 5};
 
+  print_vector(v);
+
+  const qs::stats_t stats = qs::sort(v, std::less<>());
+
+  print_vector(v);
+
+  // print statistics
+  std::cout << "Statistics:" << std::endl
+            << "  swaps:        " << stats.swaps << std::endl
+            << "  comparisons:  " << stats.comparisons << std::endl
+            << "  max depth:    " << stats.max_depth << std::endl;
 
   // =====  Your code ends here  =====
 
